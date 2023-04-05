7e80a550d8ae4beefe211ed6b5642205bc99bd95
Wang Xiaoyi <Mr_Wangxiaoyi@foxmail.com>
2023-04-05 14:39:56 +0800

    2023.4.5 1439

--- src/main/java/com/example/talis/controller/uploadController.java
+++ src/main/java/com/example/talis/controller/uploadController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/upload")
 public class uploadController {
+    //存储在本地
     @PostMapping
     public Result upload(String username, Integer age, MultipartFile image) throws Exception{
         log.info("文件上传{}{}{}",username,age,image);
